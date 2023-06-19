3e8019133443d4ae6ac79ca3e24a6aaf9b613b5f
Patrick Callahan <patricktcallahan18@gmail.com>
2023-06-19 20:42:33 +0200

    Add some comments on the key components needed to pass data around

--- R/mod_test_pass_data.R
+++ R/mod_test_pass_data.R
@@ -29,3 +29,17 @@ mod_test_pass_data_server <- function(id, prob_data){
 
 ## To be copied in the server
 # mod_test_pass_data_server("test_pass_data_1")
+
+
+
+# Code that finally helped me figure it out. Three key parts:
+# 1. return() the reactive data from the server generating the data.
+# In this case, return(probability_data) from mod_data_entry_server.
+# 2. Within app_server(), save mod_data_entry_server("id") to an object.
+# !!It will still be able to provide output to the UI even like this!!
+# 3. In the server module needing access to the data, expose an argument
+# to pass through the object saved in the previous step. (Note that the
+# argument passed through need to be treated as a function i.e. arg().)
+# https://stackoverflow.com/questions/58899369/reactive-shiny-modules-sharing-data
+
+
