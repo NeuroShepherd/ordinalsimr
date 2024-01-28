10f825392adecc45970dde7603c943e48573d9b5
Patrick Callahan <patricktcallahan18@gmail.com>
2024-01-28 17:37:15 +0100

    Generate table with distribution statistics

--- R/mod_plot_distributions.R
+++ R/mod_plot_distributions.R
@@ -13,11 +13,15 @@ mod_plot_distributions_ui <- function(id){
     box(
       width = 3,
       sliderInput(ns("remove_outlier_percentage"), "Filter out top % of observations",
-                  min = 0, max = 100, value = 10, round = TRUE)
+                  min = 0, max = 100, value = 10, round = TRUE),
+      numericInput(ns("user_p_val"), "p-value Threshold",
+                   min = 0, max = 1, value = 0.05)
     ),
     box(
       width = 9,
-      plotOutput(ns("distribution_plot_results"))
+      plotOutput(ns("distribution_plot_results")),
+      br(),
+      DT::dataTableOutput(ns("distribution_statistics"))
     )
   )
 }
@@ -30,16 +34,29 @@ mod_plot_distributions_server <- function(id, p_value_table){
     ns <- session$ns
 
     outlier_percent_removal <- reactive({ (100 - input$remove_outlier_percentage)/100 })
+    p_val_threshold <- reactive({ input$user_p_val })
 
-    p_value_reactive_table <- reactive({ p_value_table })
+    p_value_reactive_table <- reactive({ as.data.frame(p_value_table) })
 
     output$distribution_plot_results <- renderPlot({
       p_value_reactive_table() %>%
-        as.data.frame() %>%
         pivot_longer(cols = everything(),names_to = "test_name") %>%
         slice_min(order_by = value, prop = outlier_percent_removal() ) %>%
         ggplot(aes(x = value, fill = test_name )) +
-        geom_density(alpha = 0.5, color = "black")
+        geom_density(alpha = 0.5, color = "black") +
+        geom_vline(xintercept = p_val_threshold(), linetype = "dashed", size = 2)
+    })
+
+
+    output$distribution_statistics <- DT::renderDataTable({
+      p_value_reactive_table() %>%
+        pivot_longer(cols = everything(), names_to = "test_name") %>%
+        mutate(greater_than_threshold = value < p_val_threshold()) %>%
+        group_by(test_name) %>%
+        summarize("avg p-value" = mean(value),
+                  "std. error" = sd(value)/n(),
+                  "percent below threshold" = mean(greater_than_threshold)
+                  )
     })
 
 
