5ea0b29ca97af1d8df476773f259584ea2a9e37b
Patrick Callahan <patricktcallahan18@gmail.com>
2024-02-18 20:54:58 +0100

    Create basic homepage sourced from a markdown file

--- R/app_ui.R
+++ R/app_ui.R
@@ -16,14 +16,18 @@ app_ui <- function(request) {
 
       shinydashboardPlus::dashboardSidebar(
         sidebarMenu(
+          menuItem("Home", tabName = "homeinfo_page", icon = icon("book")),
           menuItem("Simulation", tabName = "simulation_page", icon = icon("sliders")),
           menuItem("Distributions", tabName = "distributions_page", icon = icon("chart-simple")),
-          menuItem("Report", tabName = "report_page", icon = icon("book"))
+          menuItem("Report", tabName = "report_page", icon = icon("file"))
         )
       ),
 
       dashboardBody(
         tabItems(
+          tabItem(tabName = "homeinfo_page",
+                  mod_homepage_ui("homepage_1")
+                  ),
           tabItem(tabName = "simulation_page",
                   fluidRow(
                     box(width = 3,
@@ -32,11 +36,11 @@ app_ui <- function(request) {
                         mod_sample_probabilities_ui("sample_probabilities_1")),
                     box(width = 9,
                         mod_data_entry_ui("data_entry_1"))
-                  ),
+                    ),
                   fluidRow(
                     mod_stats_calculations_ui("stats_calculations_1")
-                  )
-          ),
+                    )
+                  ),
 
           tabItem(tabName = "distributions_page",
                   mod_plot_distributions_ui("plot_distributions_1")
---
+++ R/mod_homepage.R
@@ -0,0 +1,37 @@
+#' homepage UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_homepage_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    fluidRow(
+      column(2),
+      column(10,
+      includeMarkdown(system.file("homepage_md.md", package = "ordinalsimr"))
+      )
+
+    )
+  )
+}
+
+#' homepage Server Functions
+#'
+#' @noRd
+mod_homepage_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+  })
+}
+
+## To be copied in the UI
+# mod_homepage_ui("homepage_1")
+
+## To be copied in the server
+# mod_homepage_server("homepage_1")
