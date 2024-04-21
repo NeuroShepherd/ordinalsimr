e5a1edd4d5f46e8daa31b18ee7ade8da3ec7751e
Patrick Callahan <patricktcallahan18@gmail.com>
2024-04-21 21:00:33 +0200

    Remove references to *_formatted.rda data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Simulation Data for Two Groups
 #'
-#' Simulated p-values and metadata for a two group comparison. See `simulation_data_two_groups_formatted` for a tidy, recommended version of this same data. Useful for Type II error and power calculations.
+#' Simulated p-values and metadata for a two group comparison. Useful for Type II error and power calculations.
 #'
 #' @format ## `simulation_data_two_groups`
 #' A list
@@ -14,7 +14,7 @@
 
 #' Simulation Data for One Group
 #'
-#' Simulated p-values and metadata for a two group comparison. See `simulation_data_one_group_formatted` for a tidy, recommended version of this same data. Useful for Type I error calculations.
+#' Simulated p-values and metadata for a two group comparison. Useful for Type I error calculations.
 #'
 #' @format ## `simulation_data_one_group`
 #' A list
