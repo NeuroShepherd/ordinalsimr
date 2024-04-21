fa6d670ad88049a946de7f5b6aea6e566ae05ea4
Patrick Callahan <patricktcallahan18@gmail.com>
2024-04-21 09:10:00 +0200

    Remove and update package sample data
    Rerun the data simulation procedure with the formatting changes made to the run_simulations function, and deleted the data/*_formatted.rda files as they are no longer needed.

--- R/data.R
+++ R/data.R
@@ -11,18 +11,6 @@
 
 "simulation_data_two_groups"
 
-#' Formatted Simulation Data for Two Groups
-#'
-#' Simulated p-values and metadata for a two group comparison in a tidy format. Useful for Type II error and power calculations.
-#'
-#' @format ## `simulation_data_two_groups_formatted`
-#' A list
-#' \describe{
-#'   \item{p_values}{A data frame of p-values from each run of each test}
-#'   \item{initial_groups}{A nested list with information for each simulation run}
-#' }
-
-"simulation_data_two_groups_formatted"
 
 #' Simulation Data for One Group
 #'
@@ -37,15 +25,3 @@
 
 "simulation_data_one_group"
 
-#' Formatted Simulation Data for One Group
-#'
-#' Simulated p-values and metadata for a two group comparison in a tidy format. Useful for Type I error calculations.
-#'
-#' @format ## `simulation_data_one_group_formatted`
-#' A list
-#' \describe{
-#'   \item{p_values}{A data frame of p-values from each run of each test}
-#'   \item{initial_groups}{A nested list with information for each simulation run}
-#' }
-
-"simulation_data_one_group_formatted"
