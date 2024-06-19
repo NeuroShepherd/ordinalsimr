65f1444d6d8943cf1f91eb1145f53e0d1d1ed3f2
Patrick Callahan <patricktcallahan18@gmail.com>
2024-06-19 11:58:38 +0200

    Correctly pass through rng info to group assignment fxn

--- R/run_simulations.R
+++ R/run_simulations.R
@@ -46,7 +46,10 @@ run_simulations <- function(sample_size, sample_prob, prob0, prob1, niter,
                initial_groups <- purrr::map(1:niter, ~assign_groups(sample_size = sample_size_nested,
                                                                     sample_prob = sample_prob,
                                                                     prob0 = prob0, prob1 = prob1,
-                                                                    seed = .x)   )
+                                                                    seed = .x,
+                                                                    .rng_kind = .rng_kind,
+                                                                    .rng_normal_kind = .rng_normal_kind,
+                                                                    .rng_sample_kind = .rng_sample_kind))
 
                p_values <- initial_groups %>%
                  sapply(.,function(x) ordinal_tests(x[["x"]], x[["y"]])) %>%
