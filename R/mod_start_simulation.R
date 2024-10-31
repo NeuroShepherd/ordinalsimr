02ee421f54159388a23a2da5d25166ec7e8f53c3
Patrick Callahan <patricktcallahan18@gmail.com>
2024-10-31 07:59:11 +0100

    Style the Run Tests button

--- R/mod_start_simulation.R
+++ R/mod_start_simulation.R
@@ -10,7 +10,8 @@
 mod_start_simulation_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    actionButton(ns("run_button"), "Run Tests")
+    actionButton(ns("run_button"), "Run Tests") %>%
+      tagAppendAttributes(class="btn btn-success")
   )
 }
 
