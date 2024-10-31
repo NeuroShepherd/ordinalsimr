f70bdf0ef6f7950ce3a5bd2169a1e3c891bca5b1
Patrick Callahan <patricktcallahan18@gmail.com>
2024-10-31 10:11:44 +0100

    Add titles for the input pill tabs

--- R/mod_simulation_inputs_page.R
+++ R/mod_simulation_inputs_page.R
@@ -30,15 +30,12 @@ mod_simulation_inputs_page_ui <- function(id) {
           mod_data_entry_ui("data_entry_1")
         )
       ),
-
-
       navset_card_tab(
         full_screen = TRUE,
         title = "Inputs",
-
         nav_panel(
           "Core Inputs",
-          card_title("A card title"),
+          card_title("Iterations, Samples, and Tests"),
           layout_column_wrap(
             width = 1/3,
               mod_iterations_ui("iterations_1"),
@@ -53,32 +50,20 @@ mod_simulation_inputs_page_ui <- function(id) {
 
         nav_panel(
           "Type I Erorr",
+          card_title("TI Error by Group"),
           mod_type_one_error_ui("type_one_error_1")
         ),
 
         nav_panel(
           "RNG Options",
+          card_title("Advanced: Random Number Generator Adjustments"),
           markdown("These Random Number Generators are advanced options, and they use the default values employed by R as of version 4.4. Run `?RNGkind` in an R session to see the associated help file."),
           layout_column_wrap(
             width = 1/3,
             !!!mod_rng_option_ui("rng_option_1")
           )
-        ),
-
-        nav_panel(
-          shiny::icon("circle-info"),
-          markdown("Learn more about [htmlwidgets](http://www.htmlwidgets.org/)")
         )
       )
-
-
-
-
-      # navset_card_pill(
-      #   title = "Simulation p-values",
-      #   !!!mod_stats_calculations_ui("stats_calculations_1")
-      # )
-
     )
   )
 }
