6846ddfccd22e01aee1dbc36eab0daec0309c8e0
Patrick Callahan <patricktcallahan18@gmail.com>
2024-10-31 13:12:18 +0100

    Create options on load if not already set

---
+++ R/zzz.R
@@ -0,0 +1,30 @@
+#' .onLoad
+#'
+#' @description
+#'
+#' This function is called when the package is loaded. It sets the default options for the package.
+#'
+#' @param libname libname
+#' @param pkgname package
+#'
+#' @return invisible
+#' @export
+#'
+.onLoad <- function(libname, pkgname) {
+
+  op <- options()
+  op.ordinalsimr <- list(
+    list(
+      ordinalsimr.default_iterations = 1000,
+      ordinalsimr.default_size_min = 30,
+      ordinalsimr.default_size_max = 200,
+      ordinalsimr.default_ratio = "50:50"
+      )
+  )
+
+  toset <- !(names(op.ordinalsimr) %in% names(op))
+  if (any(toset)) options(op.ordinalsimr[toset])
+
+  invisible()
+
+}
