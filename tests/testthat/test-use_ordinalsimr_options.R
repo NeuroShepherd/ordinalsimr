7f2e070d6ba1e31ab3fa284e6dcaf1bde3849be9
Patrick Callahan <patricktcallahan18@gmail.com>
2024-11-07 13:58:37 +0100

    Write tests for get and set fxns

---
+++ tests/testthat/test-use_ordinalsimr_options.R
@@ -0,0 +1,105 @@
+
+
+# write tests for get_ordinalsimr_options() function
+
+test_that("get_ordinalsimr_options() returns a list of ordinalsimr options", {
+
+  # set options in a withr with_options environment
+  withr::with_options(list(ordinalsimr.default_iterations = 500,
+                        ordinalsimr.default_size_min = 10,
+                        ordinalsimr.default_size_max = 100,
+                        ordinalsimr.default_ratio = "50:50",
+                        ordinalsimr.default_distributions = data.frame(c(0.4, 0.3, 0.3), c(0.8, 0.1, 0.1)),
+                        ordinalsimr.default_entry_rows = 5), {
+
+
+    options <- get_ordinalsimr_options()
+
+    expect_true(is.list(options))
+    expect_equal(length(options), 6)
+    expect_equal(names(options), c("ordinalsimr.default_iterations",
+                                   "ordinalsimr.default_size_min",
+                                   "ordinalsimr.default_size_max",
+                                   "ordinalsimr.default_ratio",
+                                   "ordinalsimr.default_distributions",
+                                   "ordinalsimr.default_entry_rows"))
+    expect_equal(options$ordinalsimr.default_iterations, 500)
+    expect_equal(options$ordinalsimr.default_size_min, 10)
+    expect_equal(options$ordinalsimr.default_size_max, 100)
+    expect_equal(options$ordinalsimr.default_ratio, "50:50")
+    expect_equal(options$ordinalsimr.default_distributions, data.frame(c(0.4, 0.3, 0.3), c(0.8, 0.1, 0.1)))
+    expect_equal(options$ordinalsimr.default_entry_rows, 5)
+
+  })
+
+
+
+})
+
+
+# write tests for setting the ordinalsimr options with the set_ordinalsimr_options() function
+
+test_that("check that set_ordinalsimr_options() works", {
+
+
+  suppressMessages({
+    # set options
+    set_ordinalsimr_options(default_iterations = 1000,
+                            default_size_min = NULL,
+                            default_size_max = NULL,
+                            default_ratio = NULL,
+                            default_distributions = NULL,
+                            default_entry_rows = 10)
+
+    # get options
+    options <- get_ordinalsimr_options()
+
+    print(options)
+
+    # check options
+    expect_true(is.list(options))
+    expect_equal(length(options), 6)
+    expect_equal(names(options), c("ordinalsimr.default_iterations",
+                                   "ordinalsimr.default_size_min",
+                                   "ordinalsimr.default_size_max",
+                                   "ordinalsimr.default_ratio",
+                                   "ordinalsimr.default_distributions",
+                                   "ordinalsimr.default_entry_rows"))
+    expect_equal(options$ordinalsimr.default_iterations, 1000)
+    expect_equal(options$ordinalsimr.default_size_min, NULL)
+    expect_equal(options$ordinalsimr.default_size_max, NULL)
+    expect_equal(options$ordinalsimr.default_ratio, NULL)
+    expect_equal(options$ordinalsimr.default_distributions, NULL)
+    expect_equal(options$ordinalsimr.default_entry_rows, 10)
+
+
+    # change the options to other values
+
+    set_ordinalsimr_options(default_iterations = 500,
+                            default_size_min = 20,
+                            default_size_max = 200,
+                            default_ratio = "60:40",
+                            default_distributions = data.frame(c(0.5, 0.3, 0.2), c(0.7, 0.2, 0.1)),
+                            default_entry_rows = 15)
+
+    # check the options again
+
+    options <- get_ordinalsimr_options()
+
+    expect_true(is.list(options))
+    expect_equal(length(options), 6)
+
+    expect_equal(options$ordinalsimr.default_iterations, 500)
+    expect_equal(options$ordinalsimr.default_size_min, 20)
+    expect_equal(options$ordinalsimr.default_size_max, 200)
+    expect_equal(options$ordinalsimr.default_ratio, "60:40")
+    expect_equal(options$ordinalsimr.default_distributions, data.frame(c(0.5, 0.3, 0.2), c(0.7, 0.2, 0.1)))
+    expect_equal(options$ordinalsimr.default_entry_rows, 15)
+
+  })
+
+
+
+})
+
+
