79927043d76b7cc2617e3a6af2559e5f51c02d62
Patrick Callahan <patricktcallahan18@gmail.com>
2024-11-08 13:26:25 +0100

    Test additional controls and print info

--- tests/testthat/test-zzz_fix1.R
+++ tests/testthat/test-zzz_fix1.R
@@ -5,8 +5,15 @@
 test_that("test .onLoad", {
 
   is_chk <- Sys.getenv("_R_CHECK_PACKAGE_NAME_", "") != .packageName
+  not_cran <- Sys.getenv("NOT_CRAN") == "true"
 
-  if (is_chk) {
+  print(paste(".packageName is", .packageName))
+  print(paste("_R_CHECK_PACKAGE_NAME_ is", Sys.getenv("_R_CHECK_PACKAGE_NAME_", "")))
+  print(paste("NOT_CRAN is", Sys.getenv("NOT_CRAN")))
+  print(paste("is_chk is", is_chk))
+  print(paste("not_cran is", not_cran))
+
+  if (is_chk && !not_cran) {
     indep_session <- callr::r(function() {
       ordinalsimr_opts_preload <- grep("ordinalsimr.", names(options()), value = TRUE) |>
         purrr::set_names() |>
@@ -41,6 +48,44 @@ test_that("test .onLoad", {
     expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_size_min, 30)
     expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_size_max, 200)
     expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_ratio, "50:50")
+  } else {
+
+
+    indep_session <- callr::r(function() {
+      ordinalsimr_opts_preload <- grep("ordinalsimr.", names(options()), value = TRUE) |>
+        purrr::set_names() |>
+        purrr::map(~getOption(.x))
+
+
+      pkgload::load_all()
+
+      ordinalsimr_opts_postload <- grep("ordinalsimr.", names(options()), value = TRUE) |>
+        purrr::set_names() |>
+        purrr::map(~getOption(.x))
+
+      return(
+        list(ordinalsimr_opts_preload = ordinalsimr_opts_preload,
+             ordinalsimr_opts_postload = ordinalsimr_opts_postload))
+    })
+
+
+
+    # check that the options are empty on starting R
+    expect_length(indep_session$ordinalsimr_opts_preload, 0)
+
+    # check that options exist after loading the package
+    expect_named(indep_session$ordinalsimr_opts_postload, c('ordinalsimr.default_iterations',
+                                                            'ordinalsimr.default_ratio',
+                                                            'ordinalsimr.default_size_max',
+                                                            'ordinalsimr.default_size_min'
+    ))
+    expect_length(indep_session$ordinalsimr_opts_postload, 4)
+
+    expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_iterations, 1000)
+    expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_size_min, 30)
+    expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_size_max, 200)
+    expect_equal(indep_session$ordinalsimr_opts_postload$ordinalsimr.default_ratio, "50:50")
+
   }
 
 
