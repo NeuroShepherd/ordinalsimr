0faca9c1be2af987a1c6442d9fcd5f6664df981e
Patrick Callahan <patricktcallahan18@gmail.com>
2024-11-09 15:52:25 +0000

    Try using testthat:::in_covr() fxn

---
+++ R/test-break-covr.R
@@ -0,0 +1,21 @@
+
+
+test_that("test R_COVR env var", {
+
+  is_chk <- Sys.getenv("_R_CHECK_PACKAGE_NAME_", "") == .packageName
+  not_cran <- Sys.getenv("NOT_CRAN") == "true"
+
+
+
+  if (testthat:::in_covr()) {
+
+    print("using load_all()")
+    pkgload::load_all()
+
+    print("using library()")
+    library(ordinalsimr)
+
+  }
+
+
+})
