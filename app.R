a2981aab360cd1a9c0c371613a1de1350bff8d1b
Patrick Callahan <patricktcallahan18@gmail.com>
2025-02-22 16:00:46 +0100

    Rely on renv for dependency installation in ShinyApps.io

--- app.R
+++ app.R
@@ -3,6 +3,6 @@
 # Or use the blue button on top of this file
 # library(pkgload)
 # pkgload::load_all(export_all = TRUE,helpers = FALSE,attach_testthat = FALSE)
-devtools::install(quick = TRUE)
+devtools::install(quick = TRUE, dependencies = FALSE, upgrade = FALSE)
 options( "golem.app.prod" = TRUE, "is.shinyapps.deployment" = TRUE)
 ordinalsimr::run_app() # add parameters here (if any)
