9faa65aa2aaecf2c5984e3f05897599db2296cf0
Patrick Callahan <patricktcallahan18@gmail.com>
2025-05-25 15:31:07 +0200

    Create ggridges plot for p-values
    Add dependency to package

--- R/utils.R
+++ R/utils.R
@@ -164,27 +164,40 @@ plot_distribution_results <- function(df, alpha = 0.05, outlier_removal = 0.10) 
     arrange(.data[["mean"]]) %>%
     pull(.data[["test_name"]])
 
+  unique_sample_sizes <- df %>%
+    pull(sample_size) %>%
+    unique()
+  sample_sizes_length <- length(unique_sample_sizes)
+  if (sample_sizes_length < 8) {
+    indices <- seq(1, sample_sizes_length)
+  } else {
+    # Select 8 evenly spaced sample sizes
+    indices <- round(seq(1, sample_sizes_length, length.out = 8))
+  }
+  selected_sample_sizes <- unique_sample_sizes[indices]
+  label_text <- c(paste("p ≥", alpha), paste("p <", 1-alpha))
+
   df %>%
     pivot_longer(cols = -all_of("sample_size"), names_to = "test_name") %>%
     mutate(test_name = factor(.data$test_name, levels = levels)) %>%
-    group_by(.data$sample_size, .data$test_name) %>%
-    summarise(value = mean(.data$value)) %>%
+    dplyr::filter(sample_size %in% selected_sample_sizes) %>%
     {
-      ggplot(., aes(
-        x = .data[["sample_size"]],
-        y = .data[["value"]],
-        color = .data[["test_name"]]
-      )) +
-        geom_line(linewidth = 2) +
-        geom_hline(yintercept = alpha, linetype = "dashed", linewidth = 1.5) +
-        expand_limits(y = 0) +
-        ggtitle("Mean p-value") +
-        labs(x = "Sample Size", y = "p-value", color = "Statistical Test") +
-        guides(fill = "none", linetype = "none") +
-        scale_x_continuous(
-          breaks = seq(0, max(df$sample_size), by = 1)
+      ggplot(., aes(x = value, y = factor(sample_size), fill = stat(x < alpha))) +
+        ggridges::geom_density_ridges_gradient(
+          scale = 3,
+          rel_min_height = 0.01,
+          quantile_lines = TRUE, quantiles = 2,
+          alpha = 0.3
         ) +
-        theme_bw() +
+        scale_fill_manual(values = c("TRUE" = "#FF00004D", "FALSE" = "#87CEEB4D"),
+                          name = "Significance (\U003B1)",
+                          labels = label_text) +
+        labs(
+          title = "P-value Distributions Across Sample Sizes",
+          x = "P-value",
+          y = "Sample Size"
+        ) +
+        theme_minimal() +
         theme(
           axis.text = element_text(face = "bold", size = 14),
           axis.title = element_text(face = "bold", size = 18),
@@ -192,8 +205,11 @@ plot_distribution_results <- function(df, alpha = 0.05, outlier_removal = 0.10) 
           axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
           axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
           legend.text = element_text(size = 14),
-          legend.title = element_text(size = 16, face = "bold")
-        )
+          legend.title = element_text(size = 16, face = "bold"),
+          legend.position = "top",
+          strip.text = element_text(face = "bold", size = 12)
+        ) +
+        facet_wrap(~ test_name, ncol = 2)
     }
 }
 
@@ -240,7 +256,7 @@ plot_power <- function(df, power_threshold = 0.80, ci_band = TRUE) {
     scale_x_continuous(
       breaks = seq(0, max(df$`Sample Size`), by = 1)
     ) +
-    theme_bw() +
+    theme_minimal() +
     theme(
       axis.text = element_text(face = "bold", size = 14),
       axis.title = element_text(face = "bold", size = 18),
